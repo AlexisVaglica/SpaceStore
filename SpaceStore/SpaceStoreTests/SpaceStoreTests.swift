24fd03515e59354a8115ca47b7d9b13a8e70c27b
avaglica <alexis.vaglica95@gmail.com>
2025-09-02 19:06:19 -0300

    [SS-01] - Application Created

---
+++ SpaceStore/SpaceStoreTests/SpaceStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpaceStoreTests.swift
+//  SpaceStoreTests
+//
+//  Created by AVaglica on 02/09/2025.
+//
+
+import Testing
+
+struct SpaceStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
